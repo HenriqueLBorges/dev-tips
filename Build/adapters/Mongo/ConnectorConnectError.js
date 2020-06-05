f344e1a4222518b21448c872124ab60e9d0e4f6a
Henrique Borges <henriquelb96@gmail.com>
2020-06-05 00:42:11 -0300

    removed Build folder

--- Build/adapters/Mongo/ConnectorConnectError.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ConnectorConnectError extends Error {
-    constructor(message) {
-        super(message);
-    }
-}
-exports.default = ConnectorConnectError;
--- Build/adapters/Mongo/ConnectorSaveError.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ConnectorSaveError extends Error {
-    constructor(message) {
-        super(message);
-    }
-}
-exports.default = ConnectorSaveError;
